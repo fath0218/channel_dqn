3bd3e435630c68484614c226e1e1c0c20dc702a6
fath0218 <gaozy218@gmail.com>
2018-04-16 02:33:44 -0700

    ramdom channel use update

--- ChannelEnv.py
+++ ChannelEnv.py
@@ -8,7 +8,10 @@ import numpy as np
 
 logger = logging.getLogger(__name__)
 
-
+RIGHT_REWARD = 10.0
+PUNISH_REWARD = -50.0
+CHANNEL_CNT = 10
+REFRESH = 1
 class ChannelEnv(gym.Env):
 	metadata = {
 		'render.modes': ['human', 'rgb_array'],
@@ -16,56 +19,42 @@ class ChannelEnv(gym.Env):
 	}
 	
 	def __init__(self):
-		self.states = [1,2,3,4,5,6,7,8,9,10] #状态空间
+		self.states = range(1,CHANNEL_CNT+1) #状态空间
 		self.x=[200,400,600,800,1000,200,400,600,800,1000]
 		self.y=[400,400,400,400,400,200,200,200,200,200]
-		self.terminate_states = dict()  #终止状态为字典格式 #需初始化及动态更新 
-		self.terminate_states[2] = 1
-		self.terminate_states[4] = 1
-		self.terminate_states[8] = 1
-		#self.terminate_states[1] = 1
-		#self.terminate_states[3] = 1
-		#self.terminate_states[5] = 1
-		#self.terminate_states[6] = 1
-		#self.terminate_states[7] = 1
-		#self.terminate_states[9] = 1
-		#self.terminate_states[10] = 1
+		
+
+		self.channel_p = [0.9, 0.01, 0.85, 0.02, 0.95, 0.99, 0.8, 0.03, 0.98, 0.99] #zuse gailv
+		
+		#终止状态为字典格式 #需初始化及动态更新
+		self.terminate_states = dict()
+		for i in range (CHANNEL_CNT):
+			ran = random.random()
+			print("channel:", i+1)
+			if ran < self.channel_p[i]:
+				print("random num:", ran)
+				print("channel_p:", self.channel_p[i])
+				print("channel blocked")
+				self.terminate_states[i+1] = 0         #this channel blocked
+			else:
+				print("random num:", ran)
+				print("channel_p:", self.channel_p[i])
+				print("channel available")
+				self.terminate_states[i+1] = 1         #this channel available
+
 		
 		self.actions = [1,2,3,4,5,6,7,8,9,10]
 
 		self.rewards = dict();        #回报的数据结构为字典
-        	#self.rewards['0-2'] = 1.0
-        	#self.rewards['0-4'] = 1.0
-		#self.rewards['0-8'] = 1.0
-		self.rewards['1-2'] = 10.0
-		self.rewards['1-4'] = 10.0
-		self.rewards['1-8'] = 10.0
-		self.rewards['3-2'] = 10.0
-		self.rewards['3-4'] = 10.0
-		self.rewards['3-8'] = 10.0
-		self.rewards['5-2'] = 10.0
-		self.rewards['5-4'] = 10.0
-		self.rewards['5-8'] = 10.0
-		self.rewards['6-2'] = 10.0
-		self.rewards['6-4'] = 10.0
-		self.rewards['6-8'] = 10.0
-		self.rewards['7-2'] = 10.0
-		self.rewards['7-4'] = 10.0
-		self.rewards['7-8'] = 10.0
-		self.rewards['9-2'] = 10.0
-		self.rewards['9-4'] = 10.0
-		self.rewards['9-8'] = 10.0
-		self.rewards['10-2'] = 10.0
-		self.rewards['10-4'] = 10.0
-		self.rewards['10-8'] = 10.0
-		
-		self.rewards['1-1'] = -5.0
-		self.rewards['3-3'] = -5.0
-		self.rewards['5-5'] = -5.0
-		self.rewards['6-6'] = -5.0
-		self.rewards['7-7'] = -5.0
-		self.rewards['9-9'] = -5.0
-		self.rewards['10-10'] = -5.0
+		for i in range (CHANNEL_CNT):
+			for j in range (CHANNEL_CNT):
+				key = "%d-%d"%(i+1, j+1)
+				if (self.terminate_states[i+1] == 0) and (self.terminate_states[j+1] == 1):
+					self.rewards[key] = RIGHT_REWARD        #available - green
+				elif (self.terminate_states[i+1] == 0) and (i==j) :
+					self.rewards[key] = PUNISH_REWARD
+				else:
+					self.rewards[key] = 0.0
 		
 		self.t = dict();             #状态转移的数据格式为字典
 		self.t['1-1'] = 1
@@ -168,6 +157,7 @@ class ChannelEnv(gym.Env):
 		self.t['8-10'] = 10
 		self.t['9-10'] = 10
 		self.t['10-10'] = 10
+
 		
 		self.gamma = 0.8         #折扣因子
 		self.viewer = None
@@ -194,8 +184,9 @@ class ChannelEnv(gym.Env):
         	#系统当前状态
 		state = self.state
 		print("present state:", state)
-		if state in self.terminate_states:
-			return state, 5.0, True, {}
+		if self.terminate_states[state]:
+			print("this is the terminal state")
+			return state, 1.0, True, {}
 		key = "%d-%d"%(state, action)   #将状态和动作组成字典的键值
 		print("key:", key)
 	    	#状态转移
@@ -210,15 +201,31 @@ class ChannelEnv(gym.Env):
 		
 		is_terminal = False
 		
-		if next_state in self.terminate_states:
+		if self.terminate_states[next_state]:
            		is_terminal = True
 			
 		if key not in self.rewards:
 			r = 0.0
 		else:
 			r = self.rewards[key]
-			
-		
+
+		#刷新信道使用状态
+		if (REFRESH):	
+			for i in range (CHANNEL_CNT):
+				ran = random.random()
+				#print("channel:", i+1)
+				if ran < self.channel_p[i]:
+					#print("random num:", ran)
+					#print("channel_p:", self.channel_p[i])
+					#print("channel blocked")
+					self.terminate_states[i+1] = 0         #this channel blocked
+				else:
+					#print("random num:", ran)
+					#print("channel_p:", self.channel_p[i])
+					#print("channel available")
+					self.terminate_states[i+1] = 1         #this channel available
+
+
 		return np.array(next_state), r,is_terminal,{}
 		
 	def reset(self):
@@ -237,74 +244,96 @@ class ChannelEnv(gym.Env):
 		if self.viewer is None:
 			from gym.envs.classic_control import rendering
 			self.viewer = rendering.Viewer(screen_width, screen_height)
-            #创建网格世界
-            #self.line1 = rendering.Line((100,300),(500,300))
-            #self.line2 = rendering.Line((100, 200), (500, 200))
-            #self.line3 = rendering.Line((100, 300), (100, 100))
-            #self.line4 = rendering.Line((180, 300), (180, 100))
-            #self.line5 = rendering.Line((260, 300), (260, 100))
-            #self.line6 = rendering.Line((340, 300), (340, 100))
-            #self.line7 = rendering.Line((420, 300), (420, 100))
-            #self.line8 = rendering.Line((500, 300), (500, 100))
-            #self.line9 = rendering.Line((100, 100), (180, 100))
-            #self.line10 = rendering.Line((260, 100), (340, 100))
-            #self.line11 = rendering.Line((420, 100), (500, 100))
+			#创建网格世界
+			#self.line1 = rendering.Line((100,300),(500,300))
             		#创建信道模型1
+			#self.chnl[0] = rendering.Line((100,300),(500,300))
 			self.chnl1 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(200,400))
 			self.chnl1.add_attr(self.circletrans)
-			self.chnl1.set_color(255,0,0)
+			if self.terminate_states[1]:
+				self.chnl1.set_color(0,255,0)        #available - green
+			else:
+				self.chnl1.set_color(255,0,0)        #blocked - red	
+
             		#创建第二个信道
 			self.chnl2 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(400,400))
 			self.chnl2.add_attr(self.circletrans)
-			self.chnl2.set_color(0,255,0)
+			if self.terminate_states[2]:
+				self.chnl2.set_color(0,255,0)        #available - green
+			else:
+				self.chnl2.set_color(255,0,0)        #blocked - red
 			
 			self.chnl3 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(600,400))
 			self.chnl3.add_attr(self.circletrans)
-			self.chnl3.set_color(255,0,0)
+			if self.terminate_states[3]:
+				self.chnl3.set_color(0,255,0)        #available - green
+			else:
+				self.chnl3.set_color(255,0,0)        #blocked - red
 			
 			self.chnl4 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(800,400))
 			self.chnl4.add_attr(self.circletrans)
-			self.chnl4.set_color(0,255,0)
+			if self.terminate_states[4]:
+				self.chnl4.set_color(0,255,0)        #available - green
+			else:
+				self.chnl4.set_color(255,0,0)        #blocked - red
 			
 			self.chnl5 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(1000,400))
 			self.chnl5.add_attr(self.circletrans)
-			self.chnl5.set_color(255,0,0)
+			if self.terminate_states[5]:
+				self.chnl5.set_color(0,255,0)        #available - green
+			else:
+				self.chnl5.set_color(255,0,0)        #blocked - red
 			
 			self.chnl6 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(200,200))
 			self.chnl6.add_attr(self.circletrans)
-			self.chnl6.set_color(255,0,0)
+			if self.terminate_states[6]:
+				self.chnl6.set_color(0,255,0)        #available - green
+			else:
+				self.chnl6.set_color(255,0,0)        #blocked - red
 			
 			self.chnl7 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(400,200))
 			self.chnl7.add_attr(self.circletrans)
-			self.chnl7.set_color(255,0,0)
+			if self.terminate_states[7]:
+				self.chnl7.set_color(0,255,0)        #available - green
+			else:
+				self.chnl7.set_color(255,0,0)        #blocked - red
 			
 			self.chnl8 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(600,200))
 			self.chnl8.add_attr(self.circletrans)
-			self.chnl8.set_color(0,255,0)
+			if self.terminate_states[8]:
+				self.chnl8.set_color(0,255,0)        #available - green
+			else:
+				self.chnl8.set_color(255,0,0)        #blocked - red
 			
 			self.chnl9 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(800,200))
 			self.chnl9.add_attr(self.circletrans)
-			self.chnl9.set_color(255,0,0)
+			if self.terminate_states[9]:
+				self.chnl9.set_color(0,255,0)        #available - green
+			else:
+				self.chnl9.set_color(255,0,0)        #blocked - red
 			
 			self.chnl10 = rendering.make_circle(50)
 			self.circletrans = rendering.Transform(translation=(1000,200))
 			self.chnl10.add_attr(self.circletrans)
-			self.chnl10.set_color(255,0,0)
+			if self.terminate_states[10]:
+				self.chnl10.set_color(0,255,0)        #available - green
+			else:
+				self.chnl10.set_color(255,0,0)        #blocked - red
 			
             		#创建机器人
 			self.robot= rendering.make_circle(30)
 			self.robotrans = rendering.Transform()
 			self.robot.add_attr(self.robotrans)
-			self.robot.set_color(255, 265, 0)
+			self.robot.set_color(255, 255, 255)
 			
 			self.viewer.add_geom(self.chnl1)
 			self.viewer.add_geom(self.chnl2)
@@ -317,6 +346,57 @@ class ChannelEnv(gym.Env):
 			self.viewer.add_geom(self.chnl9)
 			self.viewer.add_geom(self.chnl10)
 			self.viewer.add_geom(self.robot)
+
+		else:
+			if self.terminate_states[1]:
+				self.chnl1.set_color(0,255,0)        #available - green
+			else:
+				self.chnl1.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[2]:
+				self.chnl2.set_color(0,255,0)        #available - green
+			else:
+				self.chnl2.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[3]:
+				self.chnl3.set_color(0,255,0)        #available - green
+			else:
+				self.chnl3.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[4]:
+				self.chnl4.set_color(0,255,0)        #available - green
+			else:
+				self.chnl4.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[5]:
+				self.chnl5.set_color(0,255,0)        #available - green
+			else:
+				self.chnl5.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[6]:
+				self.chnl6.set_color(0,255,0)        #available - green
+			else:
+				self.chnl6.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[7]:
+				self.chnl7.set_color(0,255,0)        #available - green
+			else:
+				self.chnl7.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[8]:
+				self.chnl8.set_color(0,255,0)        #available - green
+			else:
+				self.chnl8.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[9]:
+				self.chnl9.set_color(0,255,0)        #available - green
+			else:
+				self.chnl9.set_color(255,0,0)        #blocked - red
+
+			if self.terminate_states[10]:
+				self.chnl10.set_color(0,255,0)        #available - green
+			else:
+				self.chnl10.set_color(255,0,0)        #blocked - red
 			
 			
 		
